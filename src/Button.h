3b1c52e0c7657e8ea967a6ad9c6550da1df3af63
Enes <enes515254@gmail.com>
2019-11-03 23:11:54 +0600

    Add button and update README

--- src/Button.h
+++ src/Button.h
@@ -1,3 +1,17 @@
 #include <Arduino.h>
 #include <OneButton.h>
+#include <main.h>
 
+OneButton button(BUTT_PIN, true);
+
+
+
+void click1(){
+    
+}
+
+
+
+void setupButton(){
+    button.attachClick(click1);
+}
