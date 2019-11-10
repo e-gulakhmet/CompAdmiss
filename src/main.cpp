3ed17b08f867914b5da7a9da0285f5581cc2fc71
Enes <enes515254@gmail.com>
2019-11-10 20:07:09 +0600

    Add encoder

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,59 @@
 #include <SoftwareSerial.h>
 #include <GyverRGB.h>
 #include <OneButton.h>
+#include <Wire.h>
+#include <GyverEncoder.h>
 
 #include <main.h>
 #include <Encoder.h>
-#include <Button.h>
 #include <parsing.h>
-#include <display.h>
+
 
 SoftwareSerial mySerial(TX_PIN, RX_PIN);
-GRGB leds(RED_PIN, GREEN_PIN, BLUE_PIN);  // куда подключены цвета (R, G, B)
+GRGB leds(RED_PIN, GREEN_PIN, BLUE_PIN);
+Encoder enc(ENC_A, ENC_B, BUTT_PIN, 1);
+
+
+
+
+// парсинг
+boolean onlineFlag;
+uint32_t timeoutTimer;
+char inData[82];       // массив входных значений (СИМВОЛЫ)
+int PCdata[20];        // массив численных значений показаний с компьютера
+byte index = 0;
+String string_convert;
+
+
+
+
+void parsing() {
+  if (onlineFlag && millis() - timeoutTimer > TIMEOUT) {
+    onlineFlag = false;
+  }
+  
+  while (Serial.available() > 0) {
+    char aChar = Serial.read();
+    if (aChar != 'E') {
+      inData[index] = aChar;
+      index++;
+      inData[index] = '\0';
+    } else {
+      char *p = inData;
+      char *str;
+      index = 0;
+      String value = "";
+      while ((str = strtok_r(p, ";", &p)) != NULL) {
+        string_convert = str;
+        PCdata[index] = string_convert.toInt();
+        index++;
+      }
+      index = 0;
+      timeoutTimer = millis();
+      onlineFlag = true;
+    }
+  }
+}
 
 
 
@@ -19,7 +63,6 @@ void setup() {
   mySerial.begin(9600);
   pinMode(FAN_PIN, OUTPUT);
 
-  setupEnc();
   
   leds.setBrightness(BRIGHT);
 
@@ -30,6 +73,13 @@ void setup() {
 
 void loop() {
   parsing();
-  button.tick();
+  enc.tick();
+
+  if (enc.isRight()) Serial.println("Right");         // если был поворот
+  if (enc.isLeft()) Serial.println("Left");
+  
+  if (enc.isRightH()) Serial.println("Right holded"); // если было удержание + поворот
+  if (enc.isLeftH()) Serial.println("Left holded");
 }
+
 
